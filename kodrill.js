41a9bfdbe6a721ad88ed1e5a7e8e12dca1b1c8a3
Daniel Ossipoff <ossipoff@gmail.com>
2014-09-17 15:36:36 +0200

    Create kodrill.js
    awesome

---
+++ kodrill.js
@@ -0,0 +1,13 @@
+/**
+ * Some crazy ass bullshit license/copyright notice here
+ */
+ 
+ko.drill = function(s, o) {
+    var a = s.split('.');
+    var c = ko.unwrap(o);
+    for (var i = 0; i < a.length; i++) {
+        if (c === undefined) break;
+        c = ko.unwrap(c[a[i]]);
+    }
+    return c;
+};
